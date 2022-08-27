9a7042de0643ea075b129a8ad3a2bca2f397b393
fabao <fabao@Fabao-Desktop.bbrouter>
2022-08-27 11:52:09 -0300

    Matriz

--- GitHub - JAVA/src/Teste_Matriz.java
+++ GitHub - JAVA/src/Teste_Matriz.java
@@ -7,7 +7,30 @@ public class Teste_Matriz {
 		
 		Scanner sc = new Scanner(System.in);	
 		
+		int M, N, i, j;
 		
+		System.out.print("Quantas linhas vai ter a Matriz: ");
+		M = sc.nextInt();
+		System.out.print("Quantas colunas vai ter a Matriz: ");
+		N = sc.nextInt();
+		
+		int[][] mat = new int[M][N];
+		
+		for ( i = 0; i < M; i++) {
+			for ( j = 0; j < N; j++) {
+				System.out.print("Elemento [" + i + "," + j + "]: ");
+			mat[i][j] = sc.nextInt();	
+		   }
+		}
+		
+		System.out.println();
+		System.out.println("Matriz Digitada:");
+		     for ( i = 0; i < M; i++) {
+		    	 for ( j = 0; j < N; j++) {
+		    		 System.out.println(mat[i][j] + " ");
+		        }
+	               System.out.println();		
+		   }
 		
 		
 		
