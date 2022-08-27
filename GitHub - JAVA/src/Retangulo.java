aa71251456b05e709813a43ca96f8d0e0417729e
fabao <fabao@Fabao-Desktop.bbrouter>
2022-08-27 13:02:22 -0300

    Exercicio - Retangulo

---
+++ GitHub - JAVA/src/Retangulo.java
@@ -0,0 +1,48 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Retangulo {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		
+		double base, altura, area, perimetro, diagonal;
+		
+		System.out.print("Base do retangulo: ");
+		base = sc.nextDouble();
+		System.out.print("Altura do retangulo: ");
+		altura = sc.nextDouble();
+		
+		area = base * altura;
+		perimetro = 2 * (base + altura);
+        diagonal = 	Math.sqrt(Math.pow(base,  2.0) + Math.pow(altura,  2.0)); // Math -> é o nome da classe que irá chamar ex. sqrt é para divisão// 
+        //diagonal = raiz quadrada de (base ^ 2 + altura ^ 2)//
+        
+        System.out.println("Area = " + String.format("%.4f", area));
+        System.out.println("Perimetro = " + String.format("%.4f", perimetro));
+        System.out.println("Diagonal = " + String.format("%.4f", diagonal));
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
