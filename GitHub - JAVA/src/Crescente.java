bd9793389d1077c5698114e055ebe90ff383b851
fabao <fabao@Fabao-Desktop.bbrouter>
2022-08-29 22:37:32 -0300

    Exercicio crescente

---
+++ GitHub - JAVA/src/Crescente.java
@@ -0,0 +1,32 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class Crescente {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int x, y;
+		
+		System.out.println("Digite dois numero:");
+		x = sc.nextInt();
+		y = sc.nextInt();
+		
+		while (x != y) {
+			if (x < y ) {
+				System.out.println("CRESCENTE!");
+			}
+			else {
+				System.out.println("DECRECENTE!");
+			}
+			System.out.println("DIGITE outros dois numeros ");
+			x = sc.nextInt();
+			y = sc.nextInt();
+		}
+		
+         sc.close();
+	}
+}
