8517ce0d41916faab06edc73a07802cf7b9c4077
fabao <fabao@Fabao-Desktop.bbrouter>
2022-08-31 00:39:18 -0300

    Exercicio Quadrante

---
+++ GitHub - JAVA/src/Quadrante.java
@@ -0,0 +1,42 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Quadrante {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		
+		double x, y;
+		
+		System.out.println("Digite os valores das coordenadas X e Y: ");
+		x = sc.nextDouble();
+		y = sc.nextDouble();
+		
+		while (x != 0 && y != 0) {
+			if (x > 0 && y > 0) {
+				    System.out.println("Quadrande Q1");
+			}    
+			else if (x < 0 && y > 0) {
+					System.out.println(" Quadrande Q2");
+			}
+			else if (x < 0 && y < 0) {
+					System.out.println("Quadrande Q3");
+			}
+			else {
+					System.out.println("Quadrante Q4");
+			}
+		
+		
+		System.out.println("Digite os valores das coordenadas X e Y:");
+		x = sc.nextDouble();
+		y = sc.nextDouble();
+		
+		}	
+		
+
+	}
+
+}
