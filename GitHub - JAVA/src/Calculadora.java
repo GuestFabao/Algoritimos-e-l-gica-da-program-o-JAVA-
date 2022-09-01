f8e344a1b74dfc896a24c86362fa37e12ee100ff
fabao <fabao@Fabao-Desktop.bbrouter>
2022-08-31 22:04:59 -0300

    Tabuada

---
+++ GitHub - JAVA/src/Calculadora.java
@@ -0,0 +1,33 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		
+		int N, i, resultado;
+		
+		System.out.print("Deseja a tabuada para qual valor: ");
+		N = sc.nextInt();
+		
+		for ( i = 1; i <= 10; i++) {
+		resultado = i * N;
+		System.out.println(N + " X " + i + " = " + resultado);
+		}
+				
+		
+		
+		
+		
+		
+		
+		
+	sc.close();	
+
+	}
+
+}
